00f3d0a4a3ea47ba5455a9ad465ece6b9d1a7c8a
ratik <ratikjindal21@gmail.com>
2021-03-05 20:34:19 +0530

    add yaml loader

--- templates/algob-react-boilerplate/config-overrides.js
+++ templates/algob-react-boilerplate/config-overrides.js
@@ -1,6 +1,8 @@
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+const rewireYAML = require('react-app-rewire-yaml');
 
 module.exports = function override(config, env) {
 	config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
+	config = rewireYAML(config, env);
 	return config;
 };
