cf77ba4d62f6020e273cd50d9458a5928af45d0e
ratik <ratikjindal21@gmail.com>
2021-03-30 23:19:39 +0530

    add dependency for algob

--- default/config-overrides.js
+++ default/config-overrides.js
@@ -1,9 +1,18 @@
 const rewireYAML = require('react-app-rewire-yaml');
-const { override, removeModuleScopePlugin } = require('customize-cra');
+const path = require('path');
+
+const { override, removeModuleScopePlugin, addExternalBabelPlugins } = require('customize-cra');
 
 function overrides(config, env) {
 	config = rewireYAML(config, env);
 	return config;
 };
 
-module.exports = override(overrides, removeModuleScopePlugin());
+module.exports = override(
+	overrides,
+	removeModuleScopePlugin(),
+	...addExternalBabelPlugins(
+		"@babel/plugin-proposal-optional-chaining",
+		"@babel/plugin-proposal-nullish-coalescing-operator"
+	)
+);
