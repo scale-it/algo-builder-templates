a3c37ab6f7dbb0be7523ed369799a34351ce5efd
Ratik Jindal <ratikjindal21@gmail.com>
2021-04-06 08:58:38 +0530

    Add plugins in /default to support algob import (#11)
    * add dependency for algob
    
    * update

--- default/config-overrides.js
+++ default/config-overrides.js
@@ -1,9 +1,16 @@
 const rewireYAML = require('react-app-rewire-yaml');
-const { override, removeModuleScopePlugin } = require('customize-cra');
+const { override, removeModuleScopePlugin, addExternalBabelPlugins } = require('customize-cra');
 
 function overrides(config, env) {
 	config = rewireYAML(config, env);
 	return config;
 };
 
-module.exports = override(overrides, removeModuleScopePlugin());
+module.exports = override(
+	overrides,
+	removeModuleScopePlugin(),
+	...addExternalBabelPlugins(
+		"@babel/plugin-proposal-optional-chaining",
+		"@babel/plugin-proposal-nullish-coalescing-operator"
+	)
+);
