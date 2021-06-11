6d1c21aff28f01d36fdc033743e1ffcf72b3f7a5
ratik <ratikjindal21@gmail.com>
2021-06-11 19:00:33 +0530

    update

--- default/scripts/common/common.js
+++ default/scripts/common/common.js
@@ -1,9 +1,9 @@
-const { types } = require('@algo-builder/runtime');
+const { runtime } = require('@algo-builder/algob');
 
 exports.mkParam = function (senderAccount, receiverAddr, amount, payFlags) {
   return {
-    type: types.TransactionType.TransferAlgo,
-    sign: types.SignType.SecretKey,
+    type: runtime.types.TransactionType.TransferAlgo,
+    sign: runtime.types.SignType.SecretKey,
     fromAccount: senderAccount,
     toAccountAddr: receiverAddr,
     amountMicroAlgos: amount,
