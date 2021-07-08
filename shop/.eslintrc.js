2f934d74ee34d9b615275df9471e1ba60951b061
ratik <ratikjindal21@gmail.com>
2021-07-08 10:47:05 +0530

    add basic template

---
+++ shop/.eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    es2020: true,
+    mocha: true
+  },
+  plugins: ["simple-import-sort", "sonarjs"],
+  extends: [
+    "plugin:sonarjs/recommended",
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module"
+  },
+  rules:  {
+    "import/no-extraneous-dependencies": 0,
+    "import/prefer-default-export": "off",
+    "max-classes-per-file": 0,
+    "max-len": ["error", { "code": 110, "ignoreTrailingComments": true, "ignoreStrings": true, "ignoreUrls": true, "ignoreTemplateLiterals": true}],
+    "no-underscore-dangle": 0,
+    "simple-import-sort/imports": "warn",
+    "sort-imports": "off",
+    "sonarjs/cognitive-complexity": ["error", 16],
+    "no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+    "quotes": "off",
+    "no-console": "off",
+    "no-constant-condition": ["error", { "checkLoops": false }]
+  },
+  overrides: [
+    {
+      files: [
+        '**/*.test.js',
+        '**/*.test.jsx',
+        '**/*.test.tsx',
+        '**/*.spec.js',
+        '**/*.spec.jsx',
+        '**/*.spec.tsx',
+      ],
+      env: {
+          jest: true,
+      },
+    },
+  ],
+  settings: {
+    "react": {
+      "version": "detect"
+    }
+  }
+}
