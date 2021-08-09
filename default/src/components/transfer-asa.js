e69eb9fb6cebd4c6e8badfa768c902004213c191
Amit Yadav <amy29981@gmail.com>
2021-08-09 07:48:31 +0530

    update dapp

--- default/src/components/transfer-asa.js
+++ default/src/components/transfer-asa.js
@@ -2,65 +2,25 @@
 import React from 'react';
 
 import { CHAIN_NAME } from '../algosigner.config';
+import { types, WebMode } from "@algo-builder/web";
 
-const LAST_ROUND = 'last-round';
 const CONFIRMED_ROUND = 'confirmed-round';
-
-const waitForConfirmation = async function (txId) {
-  let response = await AlgoSigner.algod({
-    ledger: CHAIN_NAME,
-    path: '/v2/status',
-  });
-  console.log(response);
-  let lastround = response[LAST_ROUND];
-  while (true) {
-    const pendingInfo = await AlgoSigner.algod({
-      ledger: CHAIN_NAME,
-      path: `/v2/transactions/pending/${txId}`,
-    });
-    if (
-      pendingInfo[CONFIRMED_ROUND] !== null &&
-      pendingInfo[CONFIRMED_ROUND] > 0
-    ) {
-      return pendingInfo;
-    }
-    lastround++;
-    await AlgoSigner.algod({
-      ledger: CHAIN_NAME,
-      path: `/v2/status/wait-for-block-after/${lastround}`,
-    });
-  }
-};
 
 async function transferASA(asaId, sndrAddr, recvAddr, amount) {
   try {
-    const txParams = await AlgoSigner.algod({
-      ledger: CHAIN_NAME,
-      path: '/v2/transactions/params',
-    });
-    const txn = {
-      fee: txParams['min-fee'],
-      firstRound: txParams[LAST_ROUND],
-      lastRound: txParams[LAST_ROUND] + 1000,
-      genesisHash: txParams['genesis-hash'],
-      genesisID: txParams['genesis-id'],
-      from: sndrAddr,
-      type: 'axfer',
-      assetIndex: asaId,
+    const web = new WebMode(AlgoSigner, CHAIN_NAME);
+    const tx = {
+      type: types.TransactionType.TransferAsset,
+      sign: types.SignType.SecretKey,
+      fromAccountAddr: sndrAddr,
+      toAccountAddr: recvAddr,
       amount: amount,
-      to: recvAddr,
+      assetID: asaId,
+      payFlags: { totalFee: 1000 }
     };
-    let signedTxn = await AlgoSigner.sign(txn);
-    console.log('SignedTx ', signedTxn);
-    let sentTx = await AlgoSigner.send({
-      ledger: CHAIN_NAME,
-      tx: signedTxn.blob,
-    });
-    console.log('Sent transaction ', sentTx);
-
-    let response = await waitForConfirmation(sentTx.txId);
+    let response = await web.executeTransaction(tx);
+    console.log(response);
     const confirmedTxInfo = {
-      txId: sentTx.txId,
       assetIndex: asaId,
       confirmedRound: response[CONFIRMED_ROUND],
     };
