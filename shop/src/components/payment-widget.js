6ea51f166496c358833ebc85772c062b84236a3e
Megha-Dev-19 <meghadev1901@gmail.com>
2022-03-07 18:52:26 +0530

    wallet connect and myalgo integrated

--- shop/src/components/payment-widget.js
+++ shop/src/components/payment-widget.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { useCallback, useState } from 'react';
 
-import { CHAIN_NAME } from '../algosigner.config';
+import { CHAIN_NAME, Wallets } from '../config';
 
 const CONFIRMED_ROUND = 'confirmed-round';
 
@@ -19,16 +19,35 @@ const CONFIRMED_ROUND = 'confirmed-round';
  * https://github.com/PureStake/algosigner/issues/252 is resolved.
  * Task - https://www.pivotaltracker.com/story/show/178760753
  */
-async function getDefaultAccountAddr() {
-  const walletAccounts =
-    (await AlgoSigner.accounts({
-      ledger: CHAIN_NAME,
-    })) ?? [];
-
-  if (walletAccounts.length) {
-    return walletAccounts[0].address;
+async function getDefaultAccountAddr (selectedWallet, response) {
+  let walletAccounts;
+  switch (selectedWallet) {
+    case Wallets.ALGOSIGNER: {
+      walletAccounts =
+        (await AlgoSigner.accounts({
+          ledger: CHAIN_NAME,
+        })) ?? [];
+      if (walletAccounts.length) {
+        return walletAccounts[0].address;
+      }
+      return undefined;
+    }
+    case Wallets.MY_ALGO: {
+      walletAccounts = response.accounts ?? []
+      if (walletAccounts.length) {
+        return walletAccounts[0].address;
+      }
+      return undefined;
+    }
+    case Wallets.WALLET_CONNECT:
+      walletAccounts = response.wcAccounts ?? [];
+      if (walletAccounts.length) {
+        return walletAccounts[0];
+      }
+      return undefined;
+    default:
+      console.log('wallet is undefined');
   }
-  return undefined;
 }
 
 /**
@@ -37,11 +56,33 @@ async function getDefaultAccountAddr() {
  * @param toAddress address to recieve payment (currently set to master-account)
  * @param amount amount (in ALGOs)
  * @param setLoading setLoading function to set loading state in button
+ * @param selectedWallet wallet through which transaction is to be executed
+ * @param connector instance of connected wallet class
  * @returns response of transaction OR rejection message
  */
-async function executePayment(fromAddress, toAddress, amount, setLoading) {
+async function executePayment (
+  fromAddress,
+  toAddress,
+  amount,
+  setLoading,
+  selectedWallet,
+  connector
+) {
+  let connect;
+  switch (selectedWallet) {
+    case Wallets.ALGOSIGNER:
+      connect = new WebMode(AlgoSigner, CHAIN_NAME);
+      break;
+    case Wallets.MY_ALGO:
+      connect = connector
+      break;
+    case Wallets.WALLET_CONNECT:
+      connect = connector
+      break;
+    default:
+      console.log('wallet is undefined');
+  }
   try {
-    const web = new WebMode(AlgoSigner, CHAIN_NAME);
     const txParams = {
       type: types.TransactionType.TransferAlgo,
       sign: types.SignType.SecretKey,
@@ -53,7 +94,7 @@ async function executePayment(fromAddress, toAddress, amount, setLoading) {
 
     // show loading state on button while we send & wait for transaction response
     setLoading(true);
-    let response = await web.executeTransaction(txParams);
+    let response = await connect.executeTransaction(txParams);
     console.log(response);
     const confirmedTxInfo = {
       txId: response.txId,
@@ -80,8 +121,15 @@ async function executePayment(fromAddress, toAddress, amount, setLoading) {
  * and a text prop to show below button (where text is the transaction response)
  * @param buttonText text to display on button
  * @param amount amount(of ticket) in ALGOs to charge user
+ * @param selectedWallet wallet through which transaction is to be executed
+ * @param connector instance of connected wallet class
  */
-export const PaymentWidget = ({ buttonText, amount }) => {
+export const PaymentWidget = ({
+  buttonText,
+  amount,
+  selectedWallet,
+  connector,
+}) => {
   const [result, setResult] = useState('');
   const [loading, setLoading] = useState(false);
 
@@ -90,7 +138,7 @@ export const PaymentWidget = ({ buttonText, amount }) => {
     // We will take payments in this address
     const toAddress =
       'WWYNX3TKQYVEREVSW6QQP3SXSFOCE3SKUSEIVJ7YAGUPEACNI5UGI4DZCE';
-    const fromAddress = await getDefaultAccountAddr();
+    const fromAddress = await getDefaultAccountAddr(selectedWallet, connector);
 
     if (fromAddress) {
       setResult('processing...');
@@ -98,14 +146,16 @@ export const PaymentWidget = ({ buttonText, amount }) => {
         fromAddress,
         toAddress,
         amount,
-        setLoading
+        setLoading,
+        selectedWallet,
+        connector
       );
       setResult(response);
     } else {
       setResult('No accounts found in wallet');
     }
     setLoading(false);
-  }, []);
+  }, [selectedWallet, connector]);
 
   return (
     <div>
@@ -119,7 +169,7 @@ export const PaymentWidget = ({ buttonText, amount }) => {
       >
         {loading ? (
           <CircularProgress
-            color="white"
+            color="primary"
             style={{
               padding: '8px 8px 8px 8px',
               margin: '-6px 24px -6px 24px',
@@ -140,9 +190,13 @@ export const PaymentWidget = ({ buttonText, amount }) => {
 PaymentWidget.propTypes = {
   buttonText: PropTypes.string,
   amount: PropTypes.number,
+  selectedWallet: PropTypes.string,
+  connector: PropTypes.object,
 };
 
 PaymentWidget.defaultProps = {
   buttonText: '',
   amount: 0,
+  selectedWallet: Wallets.ALGOSIGNER,
+  connector: {},
 };
--- shop/src/components/signer.js
+++ shop/src/components/signer.js
@@ -1,113 +1,136 @@
 /* global AlgoSigner */
+
 import './signer.css';
 
-import { Button, Container, CssBaseline, Typography } from '@material-ui/core';
-import PropTypes from 'prop-types';
+import { MyAlgoWalletSession, WallectConnectSession } from '@algo-builder/web';
+import {
+  Button,
+  Container,
+  CssBaseline,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Typography,
+} from '@material-ui/core';
 import React from 'react';
 import { useCallback, useState } from 'react';
 
-import { CHAIN_NAME } from '../algosigner.config';
+import { CHAIN_NAME, Wallets } from '../config';
 import { PaymentWidget } from './payment-widget';
 
-const ExampleAlgoSigner = ({ title, buttonText, buttonAction }) => {
-  const [result, setResult] = useState('');
+const WalletExample = () => {
+  const [result, setResult] = useState({});
+  const [error, setError] = useState('');
+  const [selectedWallet, setSelectedWallet] = useState(Wallets.ALGOSIGNER);
 
-  const check = useCallback(async () => {
-    const r = await buttonAction();
-    setResult(r);
-  }, [buttonAction]);
+  const handleWalletChange = event => {
+    setError('');
+    setSelectedWallet(event.target.value);
+  };
 
-  return (
-    <div className="algoSigner">
-      <Typography variant="h5">{title}</Typography>
-
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={check}
-        style={{
-          margin: '5px 0px 5px 0px',
-        }}
-      >
-        {buttonText}
-      </Button>
-
-      <Typography>
-        <code>{result}</code>
-      </Typography>
-    </div>
-  );
-};
-
-const Connect = () => {
   const action = useCallback(async () => {
     try {
-      const response = await AlgoSigner.connect({
-        ledger: CHAIN_NAME,
-      });
-      return JSON.stringify(response, null, 2);
+      switch (selectedWallet) {
+        case Wallets.ALGOSIGNER:
+          await AlgoSigner.connect({
+            ledger: CHAIN_NAME,
+          });
+          break;
+        case Wallets.MY_ALGO: {
+          const connector = new MyAlgoWalletSession(CHAIN_NAME)
+          await connector.connectToMyAlgo();
+          setResult(connector)
+          break;
+        }
+        case Wallets.WALLET_CONNECT: {
+          const connector = new WallectConnectSession(CHAIN_NAME);
+          await connector.create(true);
+          connector.onConnect((error, response) => console.log(error, response));
+          setResult(connector)
+          break;
+        }
+        default:
+          setError('Please select a wallet');
+      }
     } catch (e) {
       return JSON.stringify(e.message, null, 12);
     }
-  }, []);
+  }, [selectedWallet, result]);
 
   return (
-    <ExampleAlgoSigner
-      title="Connect with Algosigner"
-      buttonText="Connect"
-      buttonAction={action}
-    />
-  );
-};
-
-const Ticket = ({ title, buttonText, amount }) => {
-  return (
-    <div className="algoSigner">
-      <Typography variant="h5">{title}</Typography>
-      <PaymentWidget buttonText={buttonText} amount={amount} />
+    <div className="mb-medium">
+      <div className="mb-medium">
+        <FormControl>
+          <FormLabel id="wallet-form">Select Wallet</FormLabel>
+          <RadioGroup
+            color="primary"
+            row
+            aria-labelledby="wallet-form"
+            name="row-radio-buttons-group"
+            value={selectedWallet}
+            onChange={handleWalletChange}
+          >
+            <FormControlLabel
+              value={Wallets.ALGOSIGNER}
+              control={<Radio color="primary" />}
+              label="AlgoSigner"
+            />
+            <FormControlLabel
+              value={Wallets.WALLET_CONNECT}
+              control={<Radio color="primary" />}
+              label="Wallet Connect"
+            />
+            <FormControlLabel
+              value={Wallets.MY_ALGO}
+              control={<Radio color="primary" />}
+              label="My Algo Wallet"
+            />
+          </RadioGroup>
+        </FormControl>
+        {error && <div className="error">{error}</div>}
+        <div>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={action}
+            style={{
+              margin: '5px 0px 5px 0px',
+            }}
+          >
+            Connect
+          </Button>
+        </div>
+      </div>
+      <div className="mb-medium">
+        <Typography variant="h5">Queen concert ticket</Typography>
+        <PaymentWidget
+          buttonText="Buy (5 ALGO)"
+          amount={5e6}
+          selectedWallet={selectedWallet}
+          connector={result}
+        />
+      </div>
+      <div className="mb-medium">
+        <Typography variant="h5">Beyonce concert ticket</Typography>
+        <PaymentWidget
+          buttonText="Buy (10 ALGO)"
+          amount={10e6}
+          selectedWallet={selectedWallet}
+          connector={result}
+        />
+      </div>
     </div>
   );
 };
 
-export default function Signer() {
+export default function Signer () {
   return (
     <Container>
       <CssBaseline />
-      <Connect />
-      <Ticket
-        title="Queen concert ticket"
-        buttonText="Buy (5 ALGO)"
-        amount={5e6}
-      />
-      <Ticket
-        title="Beyonce concert ticket"
-        buttonText="Buy (10 ALGO)"
-        amount={10e6}
-      />
+      <WalletExample />
     </Container>
   );
 }
 
-ExampleAlgoSigner.propTypes = {
-  title: PropTypes.string,
-  buttonText: PropTypes.string,
-  buttonAction: PropTypes.func,
-};
-
-ExampleAlgoSigner.defaultProps = {
-  title: '',
-  buttonText: '',
-  buttonAction: null,
-};
-
-Ticket.propTypes = {
-  title: PropTypes.string,
-  buttonText: PropTypes.string,
-  amount: PropTypes.number,
-};
-
-Ticket.defaultProps = {
-  title: '',
-  buttonText: '',
-  amount: -1,
-};
