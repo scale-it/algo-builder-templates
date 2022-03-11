d6cf4a2f07beca7b1821452895dc681d3d26e018
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2022-03-11 14:55:09 +0530

    wallet connect and myalgo integrated (#28)

---
+++ shop/src/config.js
@@ -0,0 +1,7 @@
+// wallet chain name [ MainNet | TestNet | <private_net_display_name> ]
+export const CHAIN_NAME = 'TestNet';
+export const Wallets = {
+  ALGOSIGNER: 'AlgoSigner',
+  WALLET_CONNECT: 'Wallet Connect',
+  MY_ALGO: 'My Algo',
+};
