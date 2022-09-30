e28d0a1083b53f336a0590019fdc9298975dcb87
MEGHA <100185149+Megha-Dev-19@users.noreply.github.com>
2022-09-30 11:31:53 +0530

    updated dependencies

--- warcraft/dapp/src/config.js
+++ warcraft/dapp/src/config.js
@@ -1,9 +1,13 @@
 // indexer config
+const YAMLData = require("../../contracts/artifacts/scripts/deploy.js.cp.yaml");
 export const indexerToken = "";
 export const indexerServer = "http://localhost";
 export const indexerPort = 8980;
 
 export const toAddress =
-  "WK73JQQGCRQCZ5GLQHCXXB6OXIGF6SD22F5P7HPXF5PNH23YUUALUMKOZ4";
-export const assetIndex = 4;
-export const appIndex = 7;
+	"WK73JQQGCRQCZ5GLQHCXXB6OXIGF6SD22F5P7HPXF5PNH23YUUALUMKOZ4";
+
+export const assetIndex = YAMLData.default.asa["warcraft-token"].assetIndex;
+export const appIndex = Object.values(
+	YAMLData.default.ssc["stateful.py-clear.py"]
+)[0].appID;
